6b025c2cecefb1c47768cd4ead1b6f4249c35934
Eric Snow <ericsnowcurrently@gmail.com>
2022-06-02 00:12:28 +0000

    Drop a broken assert.

--- PORTAL/jobs/requests.py
+++ PORTAL/jobs/requests.py
@@ -609,7 +609,6 @@ def resolve_bench_compile_request(reqid, workdir, remote, revision, branch,
 
 #    if not branch and ref.branch == revision:
 #        revision = 'latest'
-    assert branch or ref.branch != revision, (branch, ref)
 
     meta = BenchCompileRequest(
         id=reqid,
