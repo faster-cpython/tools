0584b1bb66241859ecca887a104f7516d5674cd3
Eric Snow <ericsnowcurrently@gmail.com>
2022-07-27 21:13:55 +0000

    Fix the --config help.

--- PORTAL/jobs/__main__.py
+++ PORTAL/jobs/__main__.py
@@ -849,7 +849,7 @@ def parse_args(argv=sys.argv[1:], prog=sys.argv[0]):
     common.add_argument('-q', '--quiet', action='count', default=0)
     if add_hidden:
         common.add_argument('--config', dest='cfgfile', metavar='FILE',
-                            help='(default: ~benchmarking/BENCH/portal.json))')
+                            help='(default: ~benchmarking/BENCH/jobs.json))')
         common.add_argument('--logfile', metavar='FILE')
         args, argv = common.parse_known_args(argv)
         cfgfile = args.cfgfile
