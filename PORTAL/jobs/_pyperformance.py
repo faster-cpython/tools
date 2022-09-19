faster-cpython/tools

commit b7bae54905a11290b791f7f3f7f212ebc67f6c9c

Simplify logic

PORTAL/jobs/_pyperformance.py
@@ -3004,10 +3004,10 @@ class PyperfResultsRepo(PyperfResultsStorage):
             raise ValueError(root)
         if isinstance(remote, str):
             remote_resolved = _utils.GitHubTarget.resolve(remote, root)
+        elif not isinstance(remote_resolved, _utils.GitHubTarget):
+            raise TypeError(f'unsupported remote {remote!r}')
         else:
             remote_resolved = remote
-        if not isinstance(remote_resolved, _utils.GitHubTarget):
-            raise TypeError(f'unsupported remote {remote!r}')
         raw = remote_resolved.ensure_local(root)
 #        raw.clean()
 #        raw.switch_branch('main')

commit 8f1aa0786ed0ed87cf2ddf00aac289dd98324ca9

Fix bug introduced in b7bae54

PORTAL/jobs/_pyperformance.py
@@ -3008,7 +3008,7 @@ class PyperfResultsRepo(PyperfResultsStorage):
             raise ValueError(root)
         if isinstance(remote, str):
             remote_resolved = _utils.GitHubTarget.resolve(remote, root)
-        elif not isinstance(remote_resolved, _utils.GitHubTarget):
+        elif not isinstance(remote, _utils.GitHubTarget):
             raise TypeError(f'unsupported remote {remote!r}')
         else:
             remote_resolved = remote